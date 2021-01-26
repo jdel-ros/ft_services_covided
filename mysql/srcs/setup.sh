8d03a9bc832401d271b14c1d8e0e4509be669dac
manacca <maximenaccache@gmail.com>
2021-01-26 12:49:14 +0100

    mysql ne crash plus

--- mysql/srcs/setup.sh
+++ mysql/srcs/setup.sh
@@ -13,3 +13,10 @@ service mariadb stop
 sed -i "s|.*skip-networking.*|#skip-networking|g" /etc/my.cnf.d/mariadb-server.cnf
 service mariadb start
 mysqld --user=root
+while true;
+do
+	if ! pgrep mysql >/dev/null 2>&1 ; then
+		exit 1
+	fi
+	sleep 2
+done
